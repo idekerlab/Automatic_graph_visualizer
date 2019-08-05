a0e5cf89f263f7637f1b4c62594635da36a72d47
Mikio Shiga <mikio_mac@MikionoMacBook-Air.local>
2019-08-06 00:50:37 +0900

    init

---
+++ README.md
@@ -0,0 +1,6 @@
+# docker
+## 使い方
+このディレクトリに移動し、dockerファイルをビルドしてdockerイメージをつくる  
+`$ sudo docker build -t tensor-keras .`  
+dockerイメージからコンテナを作成して起動  
+`$ ./run_docker.sh`
---
+++ run_docker.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+sudo docker run \
+--rm -it \
+-p 8888:8888 \
+-v $(pwd):/workdir \
+-w /workdir \
+tensor-keras \
+"$@"
+# -v $(pwd):/workdir: 現在のディレクトリ(testdir)とdocker内のworkdirを繋ぐ
+# -w /workdir: docker内で初期コマンド(jupyter notebook)を動かすディレクトリを指定(/workdir)
