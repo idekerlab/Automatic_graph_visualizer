a0df7c5faa1d6042dbbc31185ea4722b00f67ddd
agis09 <mikio_mac@MikionoMacBook-Air.local>
2019-08-08 19:12:01 +0900

    mod Dockerfile

--- pytorch/.jenkins/pytorch/win-test-helpers/test_libtorch.bat
+++
@@ -1,7 +0,0 @@
-call %SCRIPT_HELPERS_DIR%\setup_pytorch_env.bat
-
-cd %TMP_DIR_WIN%\build\torch\bin
-set PATH=C:\Program Files\NVIDIA Corporation\NvToolsExt\bin\x64;%TMP_DIR_WIN%\build\torch\lib;%PATH%
-test_api.exe --gtest_filter="-IntegrationTest.MNIST*"
-
-if errorlevel 1 exit /b 1
--- run_docker.sh
+++ run_docker.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 sudo docker run \
+--runtime=nvidia \ 
 --rm -it \
 -p 8888:8888 \
 -v $(pwd):/workdir \
 -w /workdir \
-tensor-keras \
+Test \
 "$@"
 # -v $(pwd):/workdir: 現在のディレクトリ(testdir)とdocker内のworkdirを繋ぐ
 # -w /workdir: docker内で初期コマンド(jupyter notebook)を動かすディレクトリを指定(/workdir)
